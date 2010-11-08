8de775e561c573337a2b7f1fb64a8eb1fdd50f3c
OLivier <olivier_j@hotmail.com>
2010-11-08 22:05:20 +0100

    added first code

--- ExtendedEuclides/Form1.Designer.cs
+++ ExtendedEuclides/Form1.Designer.cs
@@ -28,20 +28,93 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtFirst = new System.Windows.Forms.TextBox();
+            this.txtSecond = new System.Windows.Forms.TextBox();
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnCalculate);
+            this.groupBox1.Controls.Add(this.txtSecond);
+            this.groupBox1.Controls.Add(this.txtFirst);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(13, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(395, 326);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Input";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 43);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(57, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Invers van";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(164, 46);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(27, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "mod";
+            // 
+            // txtFirst
+            // 
+            this.txtFirst.Location = new System.Drawing.Point(69, 40);
+            this.txtFirst.Name = "txtFirst";
+            this.txtFirst.Size = new System.Drawing.Size(76, 20);
+            this.txtFirst.TabIndex = 2;
+            // 
+            // txtSecond
+            // 
+            this.txtSecond.Location = new System.Drawing.Point(197, 40);
+            this.txtSecond.Name = "txtSecond";
+            this.txtSecond.Size = new System.Drawing.Size(72, 20);
+            this.txtSecond.TabIndex = 3;
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.Location = new System.Drawing.Point(197, 82);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(75, 23);
+            this.btnCalculate.TabIndex = 4;
+            this.btnCalculate.Text = "Calculate";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(438, 288);
+            this.ClientSize = new System.Drawing.Size(604, 387);
+            this.Controls.Add(this.groupBox1);
             this.Name = "Form1";
             this.Text = "ExtendedEuclides";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox txtSecond;
+        private System.Windows.Forms.TextBox txtFirst;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnCalculate;
     }
 }
 
--- ExtendedEuclides/Form1.cs
+++ ExtendedEuclides/Form1.cs
@@ -15,5 +15,38 @@ namespace ExtendedEuclides
         {
             InitializeComponent();
         }
+
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+            calculateExtendedEuclides(int.Parse(txtFirst.Text), int.Parse(txtSecond.Text));
+
+        }
+
+        private void calculateExtendedEuclides(int b, int n)
+        {
+            int A1 = 1;
+            int A2 = 0;
+            int B1 = 0;
+            int B2 = 1;
+            int q =0 ;
+            int r =0 ;
+            int tempB1 = 0;
+            int tempB2 =0;
+
+            while ((n * A1 + b * A2)!=1)
+            {
+                q = n / b;
+                r = n % b;
+
+                B1 = (A1 - q * B1);
+                B2 = (A2 - q * B2);
+                
+                A1 = B1;
+                A2 = B2;
+
+                Console.WriteLine((n * A1 + b * A2));
+            }
+
+        }
     }
 }
