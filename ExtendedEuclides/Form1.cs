68ac1e849efbd4c084275581bea1d98e0e2e0558
OLivier <olivier_j@hotmail.com>
2010-11-09 22:15:30 +0100

    Code added, functionality implemented

--- ExtendedEuclides/Form1.cs
+++ ExtendedEuclides/Form1.cs
@@ -28,25 +28,49 @@ namespace ExtendedEuclides
             int A2 = 0;
             int B1 = 0;
             int B2 = 1;
-            int q =0 ;
-            int r =0 ;
-            int tempB1 = 0;
-            int tempB2 =0;
+            int r = n%b ;
 
-            while ((n * A1 + b * A2)!=1)
-            {
-                q = n / b;
-                r = n % b;
+            try{
+                int q = (n - r) / b;
+                int tempA1 = A1;
+                int tempA2 = A2;
 
-                B1 = (A1 - q * B1);
-                B2 = (A2 - q * B2);
-                
                 A1 = B1;
                 A2 = B2;
 
-                Console.WriteLine((n * A1 + b * A2));
-            }
+                B1 = (tempA1 - q * B1);
+                B2 = (tempA2 - q * B2);
 
+                int A = b;
+                int B = r;
+
+                while ((n * B1) + (b * B2) != 1)
+                {
+                    r = A % B;
+                    q = (A - r) / B;
+
+                    tempA1 = A1;
+                    tempA2 = A2;
+
+                    A1 = B1;
+                    A2 = B2;
+
+                    B1 = (tempA1 - q * B1);
+                    B2 = (tempA2 - q * B2);
+
+                    A = n * A1 + b * A2;
+                    B = n * B1 + b * B2;
+                    
+                }
+
+                if (B2 < 0)
+                    B2 += n;
+
+                Console.WriteLine("inverse = " + B2);
+                Console.WriteLine("rest = " + (n * B1 + b * B2));
+            }catch(Exception ex){
+                Console.WriteLine("Er is geen multiplicatief inverse");
+            }
         }
     }
 }
