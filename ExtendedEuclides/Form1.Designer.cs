f46a5266340492f75b488880a7fd661a856b59eb
OLivier <olivier_j@hotmail.com>
2010-11-11 19:16:24 +0100

    Added input validation

--- ExtendedEuclides/Form1.Designer.cs
+++ ExtendedEuclides/Form1.Designer.cs
@@ -29,12 +29,15 @@
         private void InitializeComponent()
         {
             this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.txtFirst = new System.Windows.Forms.TextBox();
-            this.txtSecond = new System.Windows.Forms.TextBox();
             this.btnCalculate = new System.Windows.Forms.Button();
+            this.txtSecond = new System.Windows.Forms.TextBox();
+            this.txtFirst = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblInvers = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
             this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
             this.SuspendLayout();
             // 
             // groupBox1
@@ -46,42 +49,10 @@
             this.groupBox1.Controls.Add(this.label1);
             this.groupBox1.Location = new System.Drawing.Point(13, 12);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(395, 326);
+            this.groupBox1.Size = new System.Drawing.Size(395, 147);
             this.groupBox1.TabIndex = 0;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Input";
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(6, 43);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(57, 13);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "Invers van";
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(164, 46);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(27, 13);
-            this.label2.TabIndex = 1;
-            this.label2.Text = "mod";
-            // 
-            // txtFirst
-            // 
-            this.txtFirst.Location = new System.Drawing.Point(69, 40);
-            this.txtFirst.Name = "txtFirst";
-            this.txtFirst.Size = new System.Drawing.Size(76, 20);
-            this.txtFirst.TabIndex = 2;
-            // 
-            // txtSecond
-            // 
-            this.txtSecond.Location = new System.Drawing.Point(197, 40);
-            this.txtSecond.Name = "txtSecond";
-            this.txtSecond.Size = new System.Drawing.Size(72, 20);
-            this.txtSecond.TabIndex = 3;
             // 
             // btnCalculate
             // 
@@ -93,16 +64,72 @@
             this.btnCalculate.UseVisualStyleBackColor = true;
             this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click);
             // 
+            // txtSecond
+            // 
+            this.txtSecond.Location = new System.Drawing.Point(197, 40);
+            this.txtSecond.Name = "txtSecond";
+            this.txtSecond.Size = new System.Drawing.Size(72, 20);
+            this.txtSecond.TabIndex = 3;
+            this.txtSecond.TextChanged += new System.EventHandler(this.txtSecond_TextChanged_1);
+            // 
+            // txtFirst
+            // 
+            this.txtFirst.Location = new System.Drawing.Point(69, 40);
+            this.txtFirst.Name = "txtFirst";
+            this.txtFirst.Size = new System.Drawing.Size(76, 20);
+            this.txtFirst.TabIndex = 2;
+            this.txtFirst.TextChanged += new System.EventHandler(this.txtFirst_TextChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(164, 46);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(27, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "mod";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 43);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(54, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Inverse of";
+            // 
+            // lblInvers
+            // 
+            this.lblInvers.AutoSize = true;
+            this.lblInvers.Location = new System.Drawing.Point(20, 35);
+            this.lblInvers.Name = "lblInvers";
+            this.lblInvers.Size = new System.Drawing.Size(0, 13);
+            this.lblInvers.TabIndex = 5;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.lblInvers);
+            this.groupBox2.Location = new System.Drawing.Point(18, 192);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(389, 171);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Output";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(604, 387);
+            this.Controls.Add(this.groupBox2);
             this.Controls.Add(this.groupBox1);
             this.Name = "Form1";
             this.Text = "ExtendedEuclides";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.groupBox1.ResumeLayout(false);
             this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -115,6 +142,8 @@
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button btnCalculate;
+        private System.Windows.Forms.Label lblInvers;
+        private System.Windows.Forms.GroupBox groupBox2;
     }
 }
 
--- ExtendedEuclides/Form1.cs
+++ ExtendedEuclides/Form1.cs
@@ -19,38 +19,21 @@ namespace ExtendedEuclides
         private void btnCalculate_Click(object sender, EventArgs e)
         {
             calculateExtendedEuclides(int.Parse(txtFirst.Text), int.Parse(txtSecond.Text));
-
         }
+        #region Calculations of the extended algorithm of euclides
+            private void calculateExtendedEuclides(int b, int n)
+            {
+                int A1 = 1;
+                int A2 = 0;
+                int B1 = 0;
+                int B2 = 1;
+                int r = n%b ;
 
-        private void calculateExtendedEuclides(int b, int n)
-        {
-            int A1 = 1;
-            int A2 = 0;
-            int B1 = 0;
-            int B2 = 1;
-            int r = n%b ;
+                try{
+                    int q = (n - r) / b;
 
-            try{
-                int q = (n - r) / b;
-                int tempA1 = A1;
-                int tempA2 = A2;
-
-                A1 = B1;
-                A2 = B2;
-
-                B1 = (tempA1 - q * B1);
-                B2 = (tempA2 - q * B2);
-
-                int A = b;
-                int B = r;
-
-                while ((n * B1) + (b * B2) != 1)
-                {
-                    r = A % B;
-                    q = (A - r) / B;
-
-                    tempA1 = A1;
-                    tempA2 = A2;
+                    int tempA1 = A1;
+                    int tempA2 = A2;
 
                     A1 = B1;
                     A2 = B2;
@@ -58,19 +41,79 @@ namespace ExtendedEuclides
                     B1 = (tempA1 - q * B1);
                     B2 = (tempA2 - q * B2);
 
-                    A = n * A1 + b * A2;
-                    B = n * B1 + b * B2;
-                    
+                    int A = b;
+                    int B = r;
+
+                    while ((n * B1) + (b * B2) != 1)
+                    {
+
+                        r = A % B;
+                        q = (A - r) / B;
+
+                        tempA1 = A1;
+                        tempA2 = A2;
+
+                        A1 = B1;
+                        A2 = B2;
+
+                        B1 = (tempA1 - q * B1);
+                        B2 = (tempA2 - q * B2);
+
+                        A = n * A1 + b * A2;
+                        B = n * B1 + b * B2;
+                        
+                    }
+
+                    if (B2 < 0)
+                        B2 += n;
+
+                    lblInvers.Text = "The multiplicative inverse is = " + B2;
+                }catch(Exception ex){
+                    lblInvers.Text = "The multiplicative inverse doesn't exist";
+                }
+            }
+        #endregion
+
+
+        #region Input validation
+                private void checkInput(Object sender)
+                {
+                    TextBox txt = sender as TextBox;
+                    int num;
+
+                    if (int.TryParse(txt.Text, out num) || txt.Text == "")
+                    {
+                        txt.BackColor = Color.White;
+                        if (txt.Text == "")
+                        {
+                            btnCalculate.Enabled = false;
+                        }
+                    }
+                    else{
+                        txt.BackColor = Color.Salmon;
+                        btnCalculate.Enabled = false;
+                    }
+
+                    if (txtFirst.Text != "" && txtSecond.Text != "" && int.TryParse(txtFirst.Text, out num) && int.TryParse(txtSecond.Text, out num))
+                    {
+                        btnCalculate.Enabled = true;
+                    }
+
+                }
+                private void txtFirst_TextChanged(object sender, EventArgs e)
+                {
+                    checkInput(sender);
                 }
 
-                if (B2 < 0)
-                    B2 += n;
+                private void txtSecond_TextChanged_1(object sender, EventArgs e)
+                {
+                    checkInput(sender);
+                }
 
-                Console.WriteLine("inverse = " + B2);
-                Console.WriteLine("rest = " + (n * B1 + b * B2));
-            }catch(Exception ex){
-                Console.WriteLine("Er is geen multiplicatief inverse");
-            }
-        }
+                private void Form1_Load(object sender, EventArgs e)
+                {
+                    btnCalculate.Enabled = false;
+                }
+        #endregion
     }
 }
